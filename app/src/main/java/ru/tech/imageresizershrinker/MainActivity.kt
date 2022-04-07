64118442fd6c24128cd43f14ee792ee7ab450af1
T8RIN <oolbp@bk.ru>
2022-04-07 10:53:45 +0300

    permissions fix

--- app/src/main/java/ru/tech/imageresizershrinker/MainActivity.kt
+++ app/src/main/java/ru/tech/imageresizershrinker/MainActivity.kt
@@ -22,11 +22,8 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
@@ -35,24 +32,22 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat.checkSelfPermission
-import androidx.core.view.WindowCompat
 import ru.tech.imageresizershrinker.ui.theme.ImageResizerShrinkerTheme
 import java.io.*
 import java.text.SimpleDateFormat
 import java.util.*
-
-private val bitmap = mutableStateOf<Bitmap?>(null)
 
 @Suppress("DEPRECATION")
 @ExperimentalMaterial3Api
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        WindowCompat.setDecorFitsSystemWindows(window, false)
 
         setContent {
             ImageResizerShrinkerTheme {
                 val ctx = LocalContext.current
+
+                val bitmap = remember { mutableStateOf<Bitmap?>(null) }
 
                 val launcher =
                     rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) { uri ->
