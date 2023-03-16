af785102111a326590d154db293b31c4ea614592
T8RIN <oolbp@bk.ru>
2023-03-16 08:39:14 +0300

    version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "ru.tech.imageresizershrinker"
         minSdk = 21
         targetSdk = 33
-        versionCode = 36
-        versionName = "1.9.1"
+        versionCode = 37
+        versionName = "1.9.2"
     }
 
     buildTypes {
