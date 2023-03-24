5db3c57fa093cf642bcaa879540e8ca15f2739a2
T8RIN <oolbp@bk.ru>
2023-03-24 10:58:17 +0300

    updated dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,18 +50,17 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-compose:1.7.0-beta01")
-    implementation("androidx.compose.material3:material3:1.1.0-alpha08")
-    implementation("androidx.compose.material3:material3-window-size-class:1.1.0-alpha08")
-    implementation("androidx.compose.material:material-icons-extended:1.4.0-rc01")
-    implementation("com.google.accompanist:accompanist-systemuicontroller:0.28.0")
+    implementation("androidx.activity:activity-compose:1.7.0")
+    implementation("androidx.compose.material3:material3:1.1.0-beta01")
+    implementation("androidx.compose.material3:material3-window-size-class:1.1.0-beta01")
+    implementation("androidx.compose.material:material-icons-extended:1.4.0")
     implementation("io.coil-kt:coil-compose:2.2.2")
     implementation("androidx.core:core-splashscreen:1.0.0")
     implementation("androidx.exifinterface:exifinterface:1.3.6")
 
     implementation("dev.olshevski.navigation:reimagined:1.3.1")
     implementation("dev.olshevski.navigation:reimagined-hilt:1.3.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
     implementation("nl.dionsegijn:konfetti-compose:2.0.2")
     implementation("androidx.datastore:datastore-preferences:1.1.0-alpha01")
     implementation("androidx.documentfile:documentfile:1.0.1")
@@ -71,7 +70,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0-alpha02")
 
     //Accompanist
-    implementation("com.google.accompanist:accompanist-placeholder-material:0.28.0")
+    implementation("com.google.accompanist:accompanist-placeholder-material:0.30.0")
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.30.0")
 
     //Coil
     implementation("io.coil-kt:coil:2.2.2")
