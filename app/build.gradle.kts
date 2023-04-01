cf9f78eb2b69ea7b88ed87c3a5b7894bf4ed1eed
T8RIN <oolbp@bk.ru>
2023-04-01 20:08:41 +0300

    1.9.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "ru.tech.imageresizershrinker"
         minSdk = 21
         targetSdk = 33
-        versionCode = 42
-        versionName = "1.9.6"
+        versionCode = 43
+        versionName = "1.9.7"
     }
 
     buildTypes {
