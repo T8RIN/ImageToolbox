833d1e7a7dbb3450553fdeb8307cc7a3969e8616
T8RIN <oolbp@bk.ru>
2023-05-17 18:06:29 +0300

    fixed bug with padding

--- app/src/main/java/ru/tech/imageresizershrinker/main_screen/MainActivity.kt
+++ app/src/main/java/ru/tech/imageresizershrinker/main_screen/MainActivity.kt
@@ -30,7 +30,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
@@ -230,7 +229,7 @@ class MainActivity : M3Activity() {
                     Surface(Modifier.fillMaxSize()) {
                         AnimatedNavHost(
                             controller = viewModel.navController,
-                            transitionSpec = { action, from, to ->
+                            transitionSpec = { action, _, _ ->
                                 fun <T> animationSpec(
                                     duration: Int = 500,
                                     delay: Int = 0
