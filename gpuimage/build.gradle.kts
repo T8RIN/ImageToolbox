c44b691b3c4d62632008d648fa4131ae06371d5c
T8RIN <oolbp@bk.ru>
2023-06-08 17:34:55 +0300

    add gpu image dep

---
+++ gpuimage/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+
+android {
+    compileSdk = 33
+
+    namespace = "jp.co.cyberagent.android.gpuimage"
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 33
+
+        ndk.abiFilters.addAll(listOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64"))
+        externalNativeBuild {
+            cmake { cppFlags("") }
+        }
+    }
+    externalNativeBuild {
+        cmake { path("src/main/cpp/CMakeLists.txt") }
+    }
+
+    buildTypes {
+        release {
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
