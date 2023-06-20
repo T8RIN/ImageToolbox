7fbb1840a7d37a3ee8470b58a34cd49e19e9a6ac
T8RIN <oolbp@bk.ru>
2023-06-21 00:16:27 +0300

    beta2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 21
         targetSdk = 33
         versionCode = 67
-        versionName = "2.2.0-beta1"
+        versionName = "2.2.0-beta2"
         resourceConfigurations += setOf(
             "en",
             "ar",
