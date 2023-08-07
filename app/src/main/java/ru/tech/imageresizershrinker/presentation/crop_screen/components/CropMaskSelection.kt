6e9cb459de02d3d4237c1701a40ca4c9eb1be1cd
T8RIN <oolbp@bk.ru>
2023-08-08 00:58:46 +0300

    clover added

--- app/src/main/java/ru/tech/imageresizershrinker/presentation/crop_screen/components/CropMaskSelection.kt
+++ app/src/main/java/ru/tech/imageresizershrinker/presentation/crop_screen/components/CropMaskSelection.kt
@@ -41,6 +41,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.asAndroidPath
 import androidx.compose.ui.graphics.asComposePath
@@ -380,7 +381,22 @@ val CloverShape: Shape = object : Shape {
         val baseWidth = 200f
         val baseHeight = 200f
 
-        val path = Paths.Clover
+        val path = Path().apply {
+            moveTo(12f, 100f)
+            cubicTo(12f, 76f, 0f, 77.6142f, 0f, 50f)
+            cubicTo(0f, 22.3858f, 22.3858f, 0f, 50f, 0f)
+            cubicTo(77.6142f, 0f, 76f, 12f, 100f, 12f)
+            cubicTo(124f, 12f, 122.3858f, 0f, 150f, 0f)
+            cubicTo(177.6142f, 0f, 200f, 22.3858f, 200f, 50f)
+            cubicTo(200f, 77.6142f, 188f, 76f, 188f, 100f)
+            cubicTo(188f, 124f, 200f, 122.3858f, 200f, 150f)
+            cubicTo(200f, 177.6142f, 177.6142f, 200f, 150f, 200f)
+            cubicTo(122.3858f, 200f, 124f, 188f, 100f, 188f)
+            cubicTo(76f, 188f, 77.6142f, 200f, 50f, 200f)
+            cubicTo(22.3858f, 200f, 0f, 177.6142f, 0f, 150f)
+            cubicTo(0f, 122.3858f, 12f, 124f, 12f, 100f)
+            close()
+        }
 
         return Outline.Generic(
             path
