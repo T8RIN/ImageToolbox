d63e8dbcee3a3b3e816e9f3415e6e2386c256ad3
T8RIN <oolbp@bk.ru>
2023-08-16 23:42:42 +0300

    f

--- app/src/main/java/ru/tech/imageresizershrinker/presentation/root/model/UiFontFam.kt
+++ app/src/main/java/ru/tech/imageresizershrinker/presentation/root/model/UiFontFam.kt
@@ -5,12 +5,54 @@ import ru.tech.imageresizershrinker.R
 import ru.tech.imageresizershrinker.domain.model.FontFam
 import ru.tech.imageresizershrinker.presentation.root.theme.fontFamilyResource
 
-open class UiFontFam private constructor(
+sealed class UiFontFam(
     val fontFamily: FontFamily,
     val name: String?
 ) {
     operator fun component1() = fontFamily
     operator fun component2() = name
+
+    data object Montserrat : UiFontFam(
+        fontFamily = fontFamilyResource(R.font.montserrat_variable),
+        name = "Montserrat"
+    )
+
+    data object Caveat : UiFontFam(
+        fontFamily = fontFamilyResource(R.font.caveat_variable),
+        name = "Caveat"
+    )
+
+    data object System : UiFontFam(FontFamily.Default, null)
+
+    data object Comfortaa : UiFontFam(
+        fontFamily = fontFamilyResource(R.font.comfortaa_varibale),
+        "Comfortaa"
+    )
+
+    data object Handjet : UiFontFam(
+        fontFamily = fontFamilyResource(R.font.handjet_varibale),
+        "Handjet"
+    )
+
+    data object YsabeauSC : UiFontFam(
+        fontFamily = fontFamilyResource(R.font.ysabeau_sc_variable),
+        "YsabeauSC"
+    )
+
+    data object Jura : UiFontFam(
+        fontFamily = fontFamilyResource(R.font.jura_variable),
+        "Jura"
+    )
+
+    data object Tektur : UiFontFam(
+        fontFamily = fontFamilyResource(R.font.tektur_variable),
+        "Tektur"
+    )
+
+    data object Podkova : UiFontFam(
+        fontFamily = fontFamilyResource(R.font.podkova_variable),
+        "Podkova"
+    )
 
     fun asDomain(): FontFam {
         return when (this) {
@@ -22,63 +64,23 @@ open class UiFontFam private constructor(
             Podkova -> FontFam.Podkova
             Tektur -> FontFam.Tektur
             YsabeauSC -> FontFam.YsabeauSC
-            else -> FontFam.Montserrat
+            Montserrat -> FontFam.Montserrat
         }
     }
 
     companion object {
-        val Montserrat = UiFontFam(
-            fontFamily = fontFamilyResource(R.font.montserrat_variable),
-            name = "Montserrat"
-        )
-
-        val Caveat = UiFontFam(
-            fontFamily = fontFamilyResource(R.font.caveat_variable),
-            name = "Caveat"
-        )
-
-        val System = UiFontFam(FontFamily.Default, null)
-
-        val Comfortaa = UiFontFam(
-            fontFamily = fontFamilyResource(R.font.comfortaa_varibale),
-            "Comfortaa"
-        )
-
-        val Handjet = UiFontFam(
-            fontFamily = fontFamilyResource(R.font.handjet_varibale),
-            "Handjet"
-        )
-
-        val YsabeauSC = UiFontFam(
-            fontFamily = fontFamilyResource(R.font.ysabeau_sc_variable),
-            "YsabeauSC"
-        )
-
-        val Jura = UiFontFam(
-            fontFamily = fontFamilyResource(R.font.jura_variable),
-            "Jura"
-        )
-
-        val Tektur = UiFontFam(
-            fontFamily = fontFamilyResource(R.font.tektur_variable),
-            "Tektur"
-        )
-
-        val Podkova = UiFontFam(
-            fontFamily = fontFamilyResource(R.font.podkova_variable),
-            "Podkova"
-        )
-
-        val entries = listOf(
-            Montserrat,
-            Caveat,
-            Comfortaa,
-            Handjet,
-            Jura,
-            Podkova,
-            Tektur,
-            YsabeauSC,
-            System
-        )
+        val entries by lazy {
+            listOf(
+                Montserrat,
+                Caveat,
+                Comfortaa,
+                Handjet,
+                Jura,
+                Podkova,
+                Tektur,
+                YsabeauSC,
+                System
+            )
+        }
     }
 }
