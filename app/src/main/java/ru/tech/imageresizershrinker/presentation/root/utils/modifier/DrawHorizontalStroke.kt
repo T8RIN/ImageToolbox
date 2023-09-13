775c93b9bf25f11fea71c5b0a2b48734cce928d1
T8RIN <oolbp@bk.ru>
2023-09-13 09:07:24 +0300

    shadows improved

--- app/src/main/java/ru/tech/imageresizershrinker/presentation/root/utils/modifier/DrawHorizontalStroke.kt
+++ app/src/main/java/ru/tech/imageresizershrinker/presentation/root/utils/modifier/DrawHorizontalStroke.kt
@@ -4,15 +4,10 @@ import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
-import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Paint
-import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -22,13 +17,12 @@ import ru.tech.imageresizershrinker.presentation.root.theme.outlineVariant
 import ru.tech.imageresizershrinker.presentation.root.widget.utils.LocalSettingsState
 
 fun Modifier.drawHorizontalStroke(top: Boolean = false, height: Dp = Dp.Unspecified) = composed {
-    val colorScheme = MaterialTheme.colorScheme
     val borderWidth = LocalSettingsState.current.borderWidth
     val h = if (height.isUnspecified) {
         borderWidth.takeIf { it > 0.dp }
     } else height
 
-    val color = colorScheme.outlineVariant(0.3f)
+    val color = MaterialTheme.colorScheme.outlineVariant(0.3f)
 
 
     if (h == null) {
@@ -45,63 +39,8 @@ fun Modifier.drawHorizontalStroke(top: Boolean = false, height: Dp = Dp.Unspecif
                 )
             }
     }
-        .then(
-            if (!top) {
-                Modifier.shadow(
-                    animateDpAsState(if (h == null) 4.dp else 0.dp).value,
-                )
-            } else {
-                val elev = animateDpAsState(if (h == null) 4.dp else 0.dp).value
-                Modifier
-                    .coloredShadow(
-                        Color.Black,
-                        alpha = 0.1f,
-                        shadowRadius = elev,
-                        offsetY = (-2).dp,
-                    )
-                    .drawWithContent {
-                        drawContent()
-                        drawRect(
-                            Color.Black.copy(0.1f),
-                            topLeft = Offset(0f, 0f),
-                            size = Size(this.size.width, 0.5.dp.toPx())
-                        )
-                    }
-            }
+        .shadow(
+            animateDpAsState(if (h == null) 6.dp else 0.dp).value
         )
         .zIndex(100f)
-}
-
-fun Modifier.coloredShadow(
-    color: Color,
-    alpha: Float = 0.2f,
-    borderRadius: Dp = 0.dp,
-    shadowRadius: Dp = 20.dp,
-    offsetY: Dp = 0.dp,
-    offsetX: Dp = 0.dp
-) = composed {
-    val shadowColor = color.copy(alpha = alpha).toArgb()
-    val transparent = color.copy(alpha = 0f).toArgb()
-    this.drawBehind {
-        this.drawIntoCanvas {
-            val paint = Paint()
-            val frameworkPaint = paint.asFrameworkPaint()
-            frameworkPaint.color = transparent
-            frameworkPaint.setShadowLayer(
-                shadowRadius.toPx(),
-                offsetX.toPx(),
-                offsetY.toPx(),
-                shadowColor
-            )
-            it.drawRoundRect(
-                0f,
-                0f,
-                this.size.width,
-                this.size.height,
-                borderRadius.toPx(),
-                borderRadius.toPx(),
-                paint
-            )
-        }
-    }
 }
