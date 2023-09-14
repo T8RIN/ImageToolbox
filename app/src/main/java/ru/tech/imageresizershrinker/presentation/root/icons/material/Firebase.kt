3938ccbf44262a59730aa976132a4d6f9a5a245c
T8RIN <oolbp@bk.ru>
2023-09-14 11:30:42 +0300

    optimised icons

--- app/src/main/java/ru/tech/imageresizershrinker/presentation/root/icons/material/Firebase.kt
+++ app/src/main/java/ru/tech/imageresizershrinker/presentation/root/icons/material/Firebase.kt
@@ -11,46 +11,38 @@ import androidx.compose.ui.graphics.vector.ImageVector.Builder
 import androidx.compose.ui.graphics.vector.path
 import androidx.compose.ui.unit.dp
 
-val Icons.Rounded.Firebase: ImageVector
-    get() {
-        if (_firebase != null) {
-            return _firebase!!
+val Icons.Rounded.Firebase: ImageVector by lazy {
+    Builder(
+        name = "Firebase", defaultWidth = 24.0.dp, defaultHeight = 24.0.dp,
+        viewportWidth = 24.0f, viewportHeight = 24.0f
+    ).apply {
+        path(
+            fill = SolidColor(Color(0xFF000000)), stroke = null, strokeLineWidth = 0.0f,
+            strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+            pathFillType = NonZero
+        ) {
+            moveTo(20.0f, 18.69f)
+            lineTo(12.7f, 22.74f)
+            curveTo(12.2f, 23.0f, 11.7f, 23.0f, 11.2f, 22.74f)
+            lineTo(4.0f, 18.69f)
+            lineTo(17.05f, 5.54f)
+            lineTo(17.4f, 5.44f)
+            curveTo(17.7f, 5.44f, 17.87f, 5.57f, 17.9f, 5.84f)
+            lineTo(20.0f, 18.69f)
+            moveTo(9.35f, 5.74f)
+            lineTo(4.8f, 13.29f)
+            lineTo(6.7f, 1.34f)
+            curveTo(6.73f, 1.07f, 6.9f, 0.94f, 7.2f, 0.94f)
+            curveTo(7.4f, 0.94f, 7.53f, 1.0f, 7.6f, 1.19f)
+            lineTo(9.75f, 5.14f)
+            lineTo(9.35f, 5.74f)
+            moveTo(13.85f, 7.0f)
+            lineTo(4.3f, 16.59f)
+            lineTo(11.55f, 4.29f)
+            curveTo(11.65f, 4.09f, 11.8f, 4.0f, 12.0f, 4.0f)
+            curveTo(12.2f, 4.0f, 12.33f, 4.09f, 12.4f, 4.29f)
+            lineTo(13.85f, 7.0f)
+            close()
         }
-        _firebase = Builder(
-            name = "Firebase", defaultWidth = 24.0.dp, defaultHeight = 24.0.dp,
-            viewportWidth = 24.0f, viewportHeight = 24.0f
-        ).apply {
-            path(
-                fill = SolidColor(Color(0xFF000000)), stroke = null, strokeLineWidth = 0.0f,
-                strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
-                pathFillType = NonZero
-            ) {
-                moveTo(20.0f, 18.69f)
-                lineTo(12.7f, 22.74f)
-                curveTo(12.2f, 23.0f, 11.7f, 23.0f, 11.2f, 22.74f)
-                lineTo(4.0f, 18.69f)
-                lineTo(17.05f, 5.54f)
-                lineTo(17.4f, 5.44f)
-                curveTo(17.7f, 5.44f, 17.87f, 5.57f, 17.9f, 5.84f)
-                lineTo(20.0f, 18.69f)
-                moveTo(9.35f, 5.74f)
-                lineTo(4.8f, 13.29f)
-                lineTo(6.7f, 1.34f)
-                curveTo(6.73f, 1.07f, 6.9f, 0.94f, 7.2f, 0.94f)
-                curveTo(7.4f, 0.94f, 7.53f, 1.0f, 7.6f, 1.19f)
-                lineTo(9.75f, 5.14f)
-                lineTo(9.35f, 5.74f)
-                moveTo(13.85f, 7.0f)
-                lineTo(4.3f, 16.59f)
-                lineTo(11.55f, 4.29f)
-                curveTo(11.65f, 4.09f, 11.8f, 4.0f, 12.0f, 4.0f)
-                curveTo(12.2f, 4.0f, 12.33f, 4.09f, 12.4f, 4.29f)
-                lineTo(13.85f, 7.0f)
-                close()
-            }
-        }
-            .build()
-        return _firebase!!
-    }
-
-private var _firebase: ImageVector? = null
+    }.build()
+}
--- app/src/main/java/ru/tech/imageresizershrinker/presentation/root/icons/material/Interface.kt
+++ app/src/main/java/ru/tech/imageresizershrinker/presentation/root/icons/material/Interface.kt
@@ -11,68 +11,60 @@ import androidx.compose.ui.graphics.vector.ImageVector.Builder
 import androidx.compose.ui.graphics.vector.path
 import androidx.compose.ui.unit.dp
 
-val Icons.Filled.Interface: ImageVector
-    get() {
-        if (_interface != null) {
-            return _interface!!
+val Icons.Filled.Interface: ImageVector by lazy {
+    Builder(
+        name = "Interface", defaultWidth = 24.0.dp, defaultHeight = 24.0.dp,
+        viewportWidth = 24.0f, viewportHeight = 24.0f
+    ).apply {
+        path(
+            fill = SolidColor(Color(0xFF000000)), stroke = null, strokeLineWidth = 0.0f,
+            strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+            pathFillType = NonZero
+        ) {
+            moveTo(6.0f, 9.0f)
+            verticalLineTo(4.0f)
+            horizontalLineTo(13.0f)
+            verticalLineTo(9.0f)
+            horizontalLineTo(23.0f)
+            verticalLineTo(16.0f)
+            horizontalLineTo(18.0f)
+            verticalLineTo(21.0f)
+            horizontalLineTo(11.0f)
+            verticalLineTo(16.0f)
+            horizontalLineTo(1.0f)
+            verticalLineTo(9.0f)
+            horizontalLineTo(6.0f)
+            moveTo(16.0f, 16.0f)
+            horizontalLineTo(13.0f)
+            verticalLineTo(19.0f)
+            horizontalLineTo(16.0f)
+            verticalLineTo(16.0f)
+            moveTo(8.0f, 9.0f)
+            horizontalLineTo(11.0f)
+            verticalLineTo(6.0f)
+            horizontalLineTo(8.0f)
+            verticalLineTo(9.0f)
+            moveTo(6.0f, 14.0f)
+            verticalLineTo(11.0f)
+            horizontalLineTo(3.0f)
+            verticalLineTo(14.0f)
+            horizontalLineTo(6.0f)
+            moveTo(18.0f, 11.0f)
+            verticalLineTo(14.0f)
+            horizontalLineTo(21.0f)
+            verticalLineTo(11.0f)
+            horizontalLineTo(18.0f)
+            moveTo(13.0f, 11.0f)
+            verticalLineTo(14.0f)
+            horizontalLineTo(16.0f)
+            verticalLineTo(11.0f)
+            horizontalLineTo(13.0f)
+            moveTo(8.0f, 11.0f)
+            verticalLineTo(14.0f)
+            horizontalLineTo(11.0f)
+            verticalLineTo(11.0f)
+            horizontalLineTo(8.0f)
+            close()
         }
-        _interface = Builder(
-            name = "Interface", defaultWidth = 24.0.dp, defaultHeight = 24.0.dp,
-            viewportWidth = 24.0f, viewportHeight = 24.0f
-        ).apply {
-            path(
-                fill = SolidColor(Color(0xFF000000)), stroke = null, strokeLineWidth = 0.0f,
-                strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
-                pathFillType = NonZero
-            ) {
-                moveTo(6.0f, 9.0f)
-                verticalLineTo(4.0f)
-                horizontalLineTo(13.0f)
-                verticalLineTo(9.0f)
-                horizontalLineTo(23.0f)
-                verticalLineTo(16.0f)
-                horizontalLineTo(18.0f)
-                verticalLineTo(21.0f)
-                horizontalLineTo(11.0f)
-                verticalLineTo(16.0f)
-                horizontalLineTo(1.0f)
-                verticalLineTo(9.0f)
-                horizontalLineTo(6.0f)
-                moveTo(16.0f, 16.0f)
-                horizontalLineTo(13.0f)
-                verticalLineTo(19.0f)
-                horizontalLineTo(16.0f)
-                verticalLineTo(16.0f)
-                moveTo(8.0f, 9.0f)
-                horizontalLineTo(11.0f)
-                verticalLineTo(6.0f)
-                horizontalLineTo(8.0f)
-                verticalLineTo(9.0f)
-                moveTo(6.0f, 14.0f)
-                verticalLineTo(11.0f)
-                horizontalLineTo(3.0f)
-                verticalLineTo(14.0f)
-                horizontalLineTo(6.0f)
-                moveTo(18.0f, 11.0f)
-                verticalLineTo(14.0f)
-                horizontalLineTo(21.0f)
-                verticalLineTo(11.0f)
-                horizontalLineTo(18.0f)
-                moveTo(13.0f, 11.0f)
-                verticalLineTo(14.0f)
-                horizontalLineTo(16.0f)
-                verticalLineTo(11.0f)
-                horizontalLineTo(13.0f)
-                moveTo(8.0f, 11.0f)
-                verticalLineTo(14.0f)
-                horizontalLineTo(11.0f)
-                verticalLineTo(11.0f)
-                horizontalLineTo(8.0f)
-                close()
-            }
-        }
-            .build()
-        return _interface!!
-    }
-
-private var _interface: ImageVector? = null
+    }.build()
+}
