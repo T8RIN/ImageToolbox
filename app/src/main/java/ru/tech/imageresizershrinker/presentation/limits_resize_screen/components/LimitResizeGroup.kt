3342e98d2262c9be63b10487eb953fd5b03bf9e2
T8RIN <oolbp@bk.ru>
2023-09-30 09:57:58 +0300

    limits RESIZE group enhanced

--- app/src/main/java/ru/tech/imageresizershrinker/presentation/limits_resize_screen/components/LimitResizeGroup.kt
+++ app/src/main/java/ru/tech/imageresizershrinker/presentation/limits_resize_screen/components/LimitResizeGroup.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import ru.tech.imageresizershrinker.R
@@ -39,8 +40,9 @@ fun LimitsResizeGroup(
                 horizontalArrangement = Arrangement.Center
             ) {
                 Text(
-                    stringResource(R.string.fallback_option),
-                    textAlign = TextAlign.Center
+                    text = stringResource(R.string.fallback_option),
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Medium
                 )
             }
             Spacer(modifier = Modifier.height(8.dp))
