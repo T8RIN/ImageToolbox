9595bdffb24c04a29bf23027710056ab693ea0f8
T8RIN <oolbp@bk.ru>
2023-10-11 13:21:18 +0300

    CHANGED TOGGLE TO GROUP BUTTON for orientation

--- app/src/main/java/ru/tech/imageresizershrinker/presentation/image_stitching_screen/components/ImageOrientationToggle.kt
+++ app/src/main/java/ru/tech/imageresizershrinker/presentation/image_stitching_screen/components/ImageOrientationToggle.kt
@@ -1,31 +1,28 @@
 package ru.tech.imageresizershrinker.presentation.image_stitching_screen.components
 
 import androidx.compose.animation.AnimatedContent
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ScreenLockLandscape
 import androidx.compose.material.icons.rounded.ScreenLockPortrait
-import androidx.compose.material.icons.rounded.ScreenRotation
 import androidx.compose.material3.Icon
-import androidx.compose.material3.LocalContentColor
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import ru.tech.imageresizershrinker.R
+import ru.tech.imageresizershrinker.presentation.root.widget.buttons.ToggleGroupButton
 import ru.tech.imageresizershrinker.presentation.root.widget.modifier.container
 
 @Composable
@@ -34,55 +31,36 @@ fun ImageOrientationToggle(
     selected: Boolean,
     onCheckedChange: (Boolean) -> Unit
 ) {
-    Row(
-        modifier = modifier
-            .container(shape = RoundedCornerShape(24.dp), resultPadding = 0.dp)
-            .clickable { onCheckedChange(!selected) }
-            .padding(horizontal = 16.dp, vertical = 8.dp),
-        verticalAlignment = Alignment.CenterVertically
+    Column(
+        modifier = Modifier
+            .container(shape = RoundedCornerShape(24.dp))
     ) {
-        Icon(Icons.Rounded.ScreenRotation, null, modifier = Modifier.defaultMinSize(24.dp, 24.dp))
-        Column(
-            Modifier
-                .weight(1f)
-                .padding(start = 16.dp, end = 16.dp, top = 6.dp, bottom = 8.dp)
-        ) {
-            Text(
-                text = stringResource(R.string.image_orientation),
-                fontWeight = FontWeight.Medium
-            )
-            AnimatedContent(selected) { isHorizontal ->
-                Text(
-                    text = if (isHorizontal) {
-                        stringResource(R.string.horizontal)
-                    } else stringResource(R.string.vertical),
-                    fontWeight = FontWeight.Normal,
-                    color = LocalContentColor.current.copy(0.5f),
-                    lineHeight = 12.sp,
-                    fontSize = 12.sp
-                )
-            }
-        }
-        Switch(
-            checked = selected,
-            onCheckedChange = onCheckedChange,
-            colors = SwitchDefaults.colors(
-                uncheckedBorderColor = MaterialTheme.colorScheme.primary,
-                uncheckedThumbColor = MaterialTheme.colorScheme.onPrimary,
-                uncheckedTrackColor = MaterialTheme.colorScheme.primary,
-                uncheckedIconColor = MaterialTheme.colorScheme.onPrimaryContainer,
-            ),
-            thumbContent = {
-                AnimatedContent(selected) { isHorizontal ->
-                    Icon(
-                        imageVector = if (isHorizontal) {
-                            Icons.Rounded.ScreenLockLandscape
-                        } else Icons.Rounded.ScreenLockPortrait,
-                        contentDescription = null,
-                        modifier = Modifier.size(SwitchDefaults.IconSize)
-                    )
+        ToggleGroupButton(
+            modifier = Modifier.padding(start = 3.dp, end = 2.dp),
+            enabled = true,
+            title = {
+                Spacer(modifier = Modifier.height(8.dp))
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Text(stringResource(id = R.string.image_orientation))
+                    Spacer(modifier = Modifier.width(8.dp))
+                    AnimatedContent(selected) { isHorizontal ->
+                        Icon(
+                            imageVector = if (isHorizontal) {
+                                Icons.Rounded.ScreenLockLandscape
+                            } else Icons.Rounded.ScreenLockPortrait,
+                            contentDescription = null,
+                            modifier = Modifier.size(SwitchDefaults.IconSize)
+                        )
+                    }
                 }
-            }
+                Spacer(modifier = Modifier.height(8.dp))
+            },
+            items = listOf(stringResource(R.string.horizontal), stringResource(R.string.vertical)),
+            selectedIndex = if (selected) 0 else 1,
+            indexChanged = { onCheckedChange(!selected) }
         )
     }
 }
