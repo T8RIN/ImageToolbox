3a661ede014c05fb5f294afa5097294ad40634a1
T8RIN <oolbp@bk.ru>
2023-11-30 17:10:09 +0300

    -

--- app/src/main/java/ru/tech/imageresizershrinker/presentation/filters_screen/components/ValueDialog.kt
+++ app/src/main/java/ru/tech/imageresizershrinker/presentation/filters_screen/components/ValueDialog.kt
@@ -30,6 +30,7 @@ import ru.tech.imageresizershrinker.R
 import ru.tech.imageresizershrinker.core.utils.trimTrailingZero
 import ru.tech.imageresizershrinker.presentation.root.widget.controls.EnhancedButton
 import ru.tech.imageresizershrinker.presentation.root.widget.modifier.alertDialogBorder
+import ru.tech.imageresizershrinker.presentation.root.widget.text.KeyboardFocusHandler
 import kotlin.math.pow
 import kotlin.math.roundToInt
 
@@ -72,6 +73,8 @@ fun ValueDialog(
                     horizontalAlignment = Alignment.CenterHorizontally,
                     verticalArrangement = Arrangement.Center
                 ) {
+                    //TODO: Remove when library will be fixed
+                    KeyboardFocusHandler()
                     OutlinedTextField(
                         shape = RoundedCornerShape(16.dp),
                         value = value,
