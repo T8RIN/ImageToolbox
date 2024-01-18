641e56de7174e35f9affa0db5492fc3150c1a0f6
T8RIN <oolbp@bk.ru>
2024-01-19 02:31:03 +0300

    added LICENCE header to each file

--- feature/image-preview/src/main/java/ru/tech/imageresizershrinker/feature/image_preview/presentation/viewModel/ImagePreviewViewModel.kt
+++ feature/image-preview/src/main/java/ru/tech/imageresizershrinker/feature/image_preview/presentation/viewModel/ImagePreviewViewModel.kt
@@ -1,3 +1,20 @@
+/*
+ * ImageToolbox is an image editor for android
+ * Copyright (c) 2024 T8RIN (Malik Mukhametzyanov)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * You should have received a copy of the Apache License
+ * along with this program.  If not, see <http://www.apache.org/licenses/LICENSE-2.0>.
+ */
+
 package ru.tech.imageresizershrinker.feature.image_preview.presentation.viewModel
 
 import android.graphics.Bitmap
@@ -8,7 +25,6 @@ import androidx.exifinterface.media.ExifInterface
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import ru.tech.imageresizershrinker.core.domain.image.ImageManager
-import ru.tech.imageresizershrinker.core.domain.model.ImageFormat
 import javax.inject.Inject
 
 @HiltViewModel
