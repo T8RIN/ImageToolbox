56c9d3342c67e917a23df35388f5717904255053
T8RIN <oolbp@bk.ru>
2024-01-23 01:13:33 +0300

    ui improved

--- feature/main/src/main/java/ru/tech/imageresizershrinker/feature/main/presentation/components/EmojiSheet.kt
+++ feature/main/src/main/java/ru/tech/imageresizershrinker/feature/main/presentation/components/EmojiSheet.kt
@@ -18,41 +18,48 @@
 package ru.tech.imageresizershrinker.feature.main.presentation.components
 
 import android.net.Uri
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Face5
-import androidx.compose.material.icons.rounded.Check
+import androidx.compose.material.icons.outlined.Face6
 import androidx.compose.material.icons.rounded.Shuffle
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import kotlinx.collections.immutable.ImmutableList
@@ -61,12 +68,11 @@ import ru.tech.imageresizershrinker.core.resources.R
 import ru.tech.imageresizershrinker.core.ui.icons.emoji.EmojiData
 import ru.tech.imageresizershrinker.core.ui.icons.emoji.EmojiItem
 import ru.tech.imageresizershrinker.core.ui.shapes.CloverShape
-import ru.tech.imageresizershrinker.core.ui.theme.blend
 import ru.tech.imageresizershrinker.core.ui.widget.buttons.EnhancedButton
 import ru.tech.imageresizershrinker.core.ui.widget.buttons.EnhancedIconButton
-import ru.tech.imageresizershrinker.core.ui.widget.controls.EnhancedSwitch
 import ru.tech.imageresizershrinker.core.ui.widget.modifier.container
-import ru.tech.imageresizershrinker.core.ui.widget.sheets.SimpleDragHandle
+import ru.tech.imageresizershrinker.core.ui.widget.other.GradientEdge
+import ru.tech.imageresizershrinker.core.ui.widget.preferences.PreferenceRowSwitch
 import ru.tech.imageresizershrinker.core.ui.widget.sheets.SimpleSheet
 import ru.tech.imageresizershrinker.core.ui.widget.text.AutoSizeText
 import ru.tech.imageresizershrinker.core.ui.widget.text.TitleItem
@@ -96,128 +102,7 @@ fun EmojiSheet(
     }
     val scope = rememberCoroutineScope()
 
-    val sheetContent: @Composable ColumnScope.() -> Unit = {
-        val alphaState by remember(emojiEnabled) {
-            derivedStateOf {
-                if (emojiEnabled) 1f else 0.4f
-            }
-        }
-        val spanModifier = Modifier
-            .padding(vertical = 16.dp)
-            .container(
-                color = MaterialTheme.colorScheme.secondaryContainer.copy(
-                    0.4f
-                ),
-                resultPadding = 0.dp
-            )
-            .padding(16.dp)
-        Column(
-            Modifier.fillMaxSize()
-        ) {
-            LazyVerticalGrid(
-                state = state,
-                columns = GridCells.Adaptive(55.dp),
-                modifier = Modifier
-                    .weight(1f)
-                    .fillMaxWidth()
-                    .alpha(
-                        animateFloatAsState(alphaState).value
-                    ),
-                userScrollEnabled = emojiEnabled,
-                verticalArrangement = Arrangement.spacedBy(
-                    6.dp,
-                    Alignment.CenterVertically
-                ),
-                horizontalArrangement = Arrangement.spacedBy(
-                    6.dp,
-                    Alignment.CenterHorizontally
-                ),
-                contentPadding = PaddingValues(start = 16.dp, end = 16.dp, bottom = 16.dp)
-            ) {
-                emojiWithCategories.forEach { (title, icon, emojis) ->
-                    item(
-                        span = { GridItemSpan(maxLineSpan) },
-                        key = icon.name
-                    ) {
-                        Row(
-                            modifier = spanModifier,
-                            verticalAlignment = Alignment.CenterVertically,
-                            horizontalArrangement = Arrangement.Center
-                        ) {
-                            Icon(icon, null, modifier = Modifier.padding(end = 16.dp))
-                            Text(title)
-                        }
-                    }
-                    emojis.forEach { emoji ->
-                        val index = allEmojis.indexOf(emoji)
-                        val selected = index == selectedEmojiIndex
-                        item(
-                            key = emoji
-                        ) {
-                            Box(
-                                modifier = Modifier
-                                    .aspectRatio(1f)
-                                    .container(
-                                        color = if (selected) MaterialTheme.colorScheme.primaryContainer
-                                        else MaterialTheme.colorScheme.secondaryContainer.copy(alpha = 0.2f),
-                                        shape = CloverShape,
-                                        borderColor = if (selected) MaterialTheme.colorScheme.onPrimaryContainer.copy(
-                                            0.7f
-                                        )
-                                        else MaterialTheme.colorScheme.onSecondaryContainer.copy(
-                                            alpha = 0.1f
-                                        ),
-                                        resultPadding = 0.dp
-                                    )
-                                    .clickable {
-                                        onEmojiPicked(index)
-                                    },
-                                contentAlignment = Alignment.Center
-                            ) {
-                                EmojiItem(
-                                    emoji = emoji.toString(),
-                                    fontSize = MaterialTheme.typography.headlineLarge.fontSize,
-                                    fontScale = 1f
-                                )
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
     SimpleSheet(
-        dragHandle = {
-            SimpleDragHandle {
-                Row(
-                    modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 8.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(stringResource(id = R.string.enable_emoji), modifier = Modifier.weight(1f))
-                    EnhancedSwitch(
-                        thumbIcon = if (emojiEnabled) {
-                            Icons.Rounded.Check
-                        } else null,
-                        checked = emojiEnabled,
-                        colors = SwitchDefaults.colors(
-                            uncheckedBorderColor = MaterialTheme.colorScheme.outline.blend(
-                                MaterialTheme.colorScheme.secondaryContainer, 0.3f
-                            ),
-                            uncheckedThumbColor = MaterialTheme.colorScheme.outline.blend(
-                                MaterialTheme.colorScheme.secondaryContainer, 0.2f
-                            ),
-                            uncheckedTrackColor = MaterialTheme.colorScheme.secondaryContainer
-                        ),
-                        onCheckedChange = {
-                            if (!emojiEnabled) onEmojiPicked(Random.nextInt(0, allEmojis.lastIndex))
-                            else onEmojiPicked(-1)
-                        }
-                    )
-                }
-            }
-        },
-        sheetContent = sheetContent,
         confirmButton = {
             Row(
                 verticalAlignment = Alignment.CenterVertically
@@ -249,5 +134,159 @@ fun EmojiSheet(
             )
         },
         visible = visible
-    )
+    ) {
+        val alphaState by remember(emojiEnabled) {
+            derivedStateOf {
+                if (emojiEnabled) 1f else 0.4f
+            }
+        }
+        val spanModifier = Modifier
+            .padding(vertical = 16.dp)
+            .container(
+                color = MaterialTheme.colorScheme.secondaryContainer.copy(
+                    0.4f
+                ),
+                resultPadding = 0.dp
+            )
+            .padding(16.dp)
+        Box {
+            val density = LocalDensity.current
+            var topPadding by remember {
+                mutableStateOf(0.dp)
+            }
+            val contentPadding by remember(topPadding) {
+                derivedStateOf {
+                    PaddingValues(
+                        start = 16.dp,
+                        end = 16.dp,
+                        bottom = 16.dp,
+                        top = topPadding
+                    )
+                }
+            }
+            Column(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                LazyVerticalGrid(
+                    state = state,
+                    columns = GridCells.Adaptive(55.dp),
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxWidth()
+                        .alpha(
+                            animateFloatAsState(alphaState).value
+                        ),
+                    userScrollEnabled = emojiEnabled,
+                    verticalArrangement = Arrangement.spacedBy(
+                        6.dp,
+                        Alignment.CenterVertically
+                    ),
+                    horizontalArrangement = Arrangement.spacedBy(
+                        6.dp,
+                        Alignment.CenterHorizontally
+                    ),
+                    contentPadding = contentPadding
+                ) {
+                    emojiWithCategories.forEach { (title, icon, emojis) ->
+                        item(
+                            span = { GridItemSpan(maxLineSpan) },
+                            key = icon.name
+                        ) {
+                            Row(
+                                modifier = spanModifier,
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.Center
+                            ) {
+                                Icon(icon, null, modifier = Modifier.padding(end = 16.dp))
+                                Text(title)
+                            }
+                        }
+                        emojis.forEach { emoji ->
+                            item(
+                                key = emoji
+                            ) {
+                                val index by remember(allEmojis, emoji) {
+                                    derivedStateOf {
+                                        allEmojis.indexOf(emoji)
+                                    }
+                                }
+                                val selected by remember(index, selectedEmojiIndex) {
+                                    derivedStateOf {
+                                        index == selectedEmojiIndex
+                                    }
+                                }
+                                val color by animateColorAsState(
+                                    if (selected) MaterialTheme.colorScheme.primaryContainer
+                                    else MaterialTheme.colorScheme.secondaryContainer.copy(
+                                        alpha = 0.2f
+                                    )
+                                )
+                                val borderColor by animateColorAsState(
+                                    if (selected) {
+                                        MaterialTheme.colorScheme.onPrimaryContainer.copy(0.7f)
+                                    } else MaterialTheme.colorScheme.onSecondaryContainer.copy(
+                                        alpha = 0.1f
+                                    )
+                                )
+                                Box(
+                                    modifier = Modifier
+                                        .aspectRatio(1f)
+                                        .container(
+                                            color = color,
+                                            shape = CloverShape,
+                                            borderColor = borderColor,
+                                            resultPadding = 0.dp
+                                        )
+                                        .clickable {
+                                            onEmojiPicked(index)
+                                        },
+                                    contentAlignment = Alignment.Center
+                                ) {
+                                    EmojiItem(
+                                        emoji = emoji.toString(),
+                                        fontSize = MaterialTheme.typography.headlineLarge.fontSize,
+                                        fontScale = 1f
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            Column(
+                modifier = Modifier.onGloballyPositioned {
+                    topPadding = with(density) {
+                        it.size.height.toDp()
+                    }
+                }
+            ) {
+                PreferenceRowSwitch(
+                    title = stringResource(R.string.enable_emoji),
+                    color = animateColorAsState(
+                        if (emojiEnabled) MaterialTheme.colorScheme.primaryContainer
+                        else MaterialTheme.colorScheme.surfaceContainerLow
+                    ).value,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(MaterialTheme.colorScheme.surfaceColorAtElevation(2.dp))
+                        .padding(start = 16.dp, top = 20.dp, bottom = 8.dp, end = 16.dp),
+                    shape = RoundedCornerShape(28.dp),
+                    checked = emojiEnabled,
+                    startIcon = Icons.Outlined.Face6,
+                    onClick = {
+                        if (!emojiEnabled) onEmojiPicked(Random.nextInt(0, allEmojis.lastIndex))
+                        else onEmojiPicked(-1)
+                    }
+                )
+                GradientEdge(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(16.dp),
+                    startColor = MaterialTheme.colorScheme.surfaceColorAtElevation(2.dp),
+                    endColor = Color.Transparent
+                )
+            }
+        }
+    }
 }
