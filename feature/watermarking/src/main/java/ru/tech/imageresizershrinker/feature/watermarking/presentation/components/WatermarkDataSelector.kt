052c4ff679ca0714cc2b3f257ecd4dc055a1ea09
T8RIN <oolbp@bk.ru>
2024-01-26 23:12:36 +0300

    watermarks improved

--- feature/watermarking/src/main/java/ru/tech/imageresizershrinker/feature/watermarking/presentation/components/WatermarkDataSelector.kt
+++ feature/watermarking/src/main/java/ru/tech/imageresizershrinker/feature/watermarking/presentation/components/WatermarkDataSelector.kt
@@ -26,7 +26,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -35,7 +34,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import ru.tech.imageresizershrinker.core.resources.R
 import ru.tech.imageresizershrinker.core.ui.icons.material.CreateAlt
@@ -72,7 +70,6 @@ fun WatermarkDataSelector(
                 )
                 .padding(8.dp),
             value = type.text,
-            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
             singleLine = false,
             onValueChange = {
                 onValueChange(
