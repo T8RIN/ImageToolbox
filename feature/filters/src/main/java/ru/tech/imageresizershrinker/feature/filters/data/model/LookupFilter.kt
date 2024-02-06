13f37292e1fa6da75ef1ef7f27f1bfdec0dc64c9
T8RIN <oolbp@bk.ru>
2024-02-07 01:20:26 +0300

    adjusted default filter values

--- feature/filters/src/main/java/ru/tech/imageresizershrinker/feature/filters/data/model/LookupFilter.kt
+++ feature/filters/src/main/java/ru/tech/imageresizershrinker/feature/filters/data/model/LookupFilter.kt
@@ -26,7 +26,7 @@ import ru.tech.imageresizershrinker.core.filters.domain.model.Filter
 
 internal class LookupFilter(
     private val context: Context,
-    override val value: Float = 1f,
+    override val value: Float = -1f,
 ) : GPUFilterTransformation(context), Filter.Lookup<Bitmap> {
 
     override val cacheKey: String
