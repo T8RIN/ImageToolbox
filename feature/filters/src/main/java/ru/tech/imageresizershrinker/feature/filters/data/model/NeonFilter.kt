337c51994e15ca2ea42a4104fdd760f6c2006584
T8RIN <oolbp@bk.ru>
2024-02-08 12:46:53 +0300

    move neon

--- feature/filters/src/main/java/ru/tech/imageresizershrinker/feature/filters/data/model/NeonFilter.kt
+++ feature/filters/src/main/java/ru/tech/imageresizershrinker/feature/filters/data/model/NeonFilter.kt
@@ -17,39 +17,30 @@
 
 package ru.tech.imageresizershrinker.feature.filters.data.model
 
+import android.content.Context
 import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.PorterDuff
-import android.graphics.PorterDuffColorFilter
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
-import androidx.core.graphics.createBitmap
 import coil.size.Size
 import ru.tech.imageresizershrinker.core.domain.image.Transformation
 import ru.tech.imageresizershrinker.core.filters.domain.model.Filter
-import ru.tech.imageresizershrinker.core.filters.domain.model.FilterValueWrapper
 import ru.tech.imageresizershrinker.core.filters.domain.model.wrap
 
-
-internal class ColorFilter(
-    override val value: FilterValueWrapper<Color> = Color.Yellow.copy(0.3f).wrap(),
-) : Filter.Color<Bitmap, Color>, Transformation<Bitmap> {
+internal class NeonFilter(
+    private val context: Context,
+    override val value: Triple<Float, Float, Color> = Triple(1f, 0.26f, Color.Magenta)
+) : Filter.Neon<Bitmap, Color>, Transformation<Bitmap> {
     override val cacheKey: String
         get() = (value).hashCode().toString()
 
-    private val Bitmap.safeConfig: Bitmap.Config
-        get() = config
-
-    override suspend fun transform(input: Bitmap, size: Size): Bitmap {
-        val output = createBitmap(input.width, input.height, input.safeConfig)
-
-        val canvas = Canvas(output)
-        val paint = Paint()
-        paint.isAntiAlias = true
-        paint.colorFilter = PorterDuffColorFilter(value.wrapped.toArgb(), PorterDuff.Mode.SRC_ATOP)
-        canvas.drawBitmap(input, 0f, 0f, paint)
-
-        return output
+    override suspend fun transform(
+        input: Bitmap,
+        size: Size
+    ): Bitmap = listOf(
+        SharpenFilter(value.second),
+        SobelEdgeDetectionFilter(context, value.first),
+        RGBFilter(context, value.third.wrap())
+    ).fold(input) { acc, filter ->
+        filter.transform(acc, size)
     }
+
 }
