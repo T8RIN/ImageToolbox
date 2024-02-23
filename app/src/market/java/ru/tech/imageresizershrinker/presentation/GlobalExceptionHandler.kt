a8c385e9e15ddcab859d93f4f72ad2d0a6ea3423
T8RIN <oolbp@bk.ru>
2024-02-23 21:49:17 +0300

    added better analytics for market version

--- app/src/market/java/ru/tech/imageresizershrinker/presentation/GlobalExceptionHandler.kt
+++ app/src/market/java/ru/tech/imageresizershrinker/presentation/GlobalExceptionHandler.kt
@@ -96,7 +96,6 @@ class GlobalExceptionHandler<T : CrashHandler> private constructor(
             if (allowCollectAnalytics) {
                 Log.d("SCREEN_OPEN", screen::class.simpleName.toString())
                 Firebase.analytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT) {
-                    param(FirebaseAnalytics.Param.ITEM_ID, screen.id.toLong())
                     param(FirebaseAnalytics.Param.CONTENT_TYPE, screen::class.simpleName.toString())
                 }
             }
