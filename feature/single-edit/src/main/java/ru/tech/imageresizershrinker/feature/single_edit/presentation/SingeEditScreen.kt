b370e7f71f25f54c366e3314c5d7bb46b4c1808e
T8RIN <oolbp@bk.ru>
2024-02-25 00:42:16 +0300

    todo

--- feature/single-edit/src/main/java/ru/tech/imageresizershrinker/feature/single_edit/presentation/SingeEditScreen.kt
+++ feature/single-edit/src/main/java/ru/tech/imageresizershrinker/feature/single_edit/presentation/SingeEditScreen.kt
@@ -218,6 +218,7 @@ fun SingleEditScreen(
             )
         },
         actions = {
+            //TODO: Create Share Button with picker of clipboard if available
             EnhancedIconButton(
                 containerColor = Color.Transparent,
                 contentColor = LocalContentColor.current,
