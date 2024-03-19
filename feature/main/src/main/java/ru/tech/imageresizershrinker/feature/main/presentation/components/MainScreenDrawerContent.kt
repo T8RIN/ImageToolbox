59c5115ca61b74c3ceee8b7d9cc508a35bf4444f
T8RIN <oolbp@bk.ru>
2024-03-19 16:25:57 +0300

    add predictive back for settings drawer

--- feature/main/src/main/java/ru/tech/imageresizershrinker/feature/main/presentation/components/MainScreenDrawerContent.kt
+++ feature/main/src/main/java/ru/tech/imageresizershrinker/feature/main/presentation/components/MainScreenDrawerContent.kt
@@ -18,6 +18,7 @@
 package ru.tech.imageresizershrinker.feature.main.presentation.components
 
 import androidx.activity.compose.BackHandler
+import androidx.activity.compose.PredictiveBackHandler
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.animateFloatAsState
@@ -32,6 +33,7 @@ import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.ArrowBack
 import androidx.compose.material.icons.automirrored.rounded.MenuOpen
@@ -50,33 +52,39 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.movableContentOf
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.coerceAtLeast
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.min
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.delay
 import ru.tech.imageresizershrinker.core.resources.R
 import ru.tech.imageresizershrinker.core.settings.presentation.LocalSettingsState
 import ru.tech.imageresizershrinker.core.ui.theme.blend
 import ru.tech.imageresizershrinker.core.ui.theme.outlineVariant
 import ru.tech.imageresizershrinker.core.ui.widget.buttons.EnhancedIconButton
+import ru.tech.imageresizershrinker.core.ui.widget.modifier.animateShape
 import ru.tech.imageresizershrinker.core.ui.widget.other.EnhancedTopAppBar
 import ru.tech.imageresizershrinker.core.ui.widget.other.EnhancedTopAppBarDefaults
 import ru.tech.imageresizershrinker.core.ui.widget.other.SearchBar
 import ru.tech.imageresizershrinker.core.ui.widget.text.Marquee
+import kotlin.coroutines.cancellation.CancellationException
 
 @Composable
 internal fun MainScreenDrawerContent(
@@ -88,7 +96,6 @@ internal fun MainScreenDrawerContent(
     settingsBlockContent: @Composable (keyword: String) -> Unit
 ) {
     val settingsState = LocalSettingsState.current
-    val scope = rememberCoroutineScope()
     var settingsSearchKeyword by rememberSaveable {
         mutableStateOf("")
     }
@@ -96,13 +103,6 @@ internal fun MainScreenDrawerContent(
     val settingsBlock = remember {
         movableContentOf {
             settingsBlockContent(settingsSearchKeyword)
-        }
-    }
-    if (sideSheetState.isOpen && isSheetSlideable) {
-        BackHandler {
-            scope.launch {
-                sideSheetState.close()
-            }
         }
     }
 
@@ -124,12 +124,85 @@ internal fun MainScreenDrawerContent(
         }
     }
 
+    var animatedScale by remember {
+        mutableFloatStateOf(1f)
+    }
+    var animatedOffsetX by remember {
+        mutableFloatStateOf(0f)
+    }
+    var animatedOffsetY by remember {
+        mutableFloatStateOf(0f)
+    }
+    var animatedShape by remember {
+        mutableStateOf(
+            RoundedCornerShape(
+                topStart = 16.dp,
+                bottomStart = 16.dp,
+                bottomEnd = 0.dp,
+                topEnd = 0.dp
+            )
+        )
+    }
+    var initialSwipeOffset by remember { mutableStateOf(Offset.Zero) }
+    val scale by animateFloatAsState(animatedScale)
+    val offsetX by animateFloatAsState(animatedOffsetX)
+    val offsetY by animateFloatAsState(animatedOffsetY)
+    val shape = animateShape(animatedShape)
+
+    val clean = {
+        animatedOffsetX = 0f
+        animatedOffsetY = 0f
+        animatedScale = 1f
+        animatedShape = RoundedCornerShape(
+            topStart = 0.dp,
+            bottomStart = 0.dp,
+            bottomEnd = 24.dp,
+            topEnd = 24.dp
+        )
+        initialSwipeOffset = Offset.Zero
+    }
+
+    LaunchedEffect(sideSheetState.isOpen) {
+        if (!sideSheetState.isOpen) {
+            delay(300L)
+            clean()
+        }
+    }
+
+    if (sideSheetState.isOpen && isSheetSlideable) {
+        PredictiveBackHandler { progress ->
+            try {
+                progress.collect { event ->
+                    if (event.progress <= 0.05f) {
+                        clean()
+                        initialSwipeOffset = Offset(event.touchX, event.touchY)
+                    }
+
+                    animatedOffsetX = initialSwipeOffset.x - event.touchX
+                    animatedOffsetY = event.touchY - initialSwipeOffset.y
+                    animatedShape = RoundedCornerShape(24.dp)
+                    animatedScale = (1f - event.progress * 2f).coerceAtLeast(0.7f)
+                }
+                sideSheetState.close()
+            } catch (e: CancellationException) {
+                clean()
+            }
+        }
+    }
+
     ModalDrawerSheet(
         modifier = Modifier
             .width(animateDpAsState(targetValue = widthState).value)
             .then(
                 if (isSheetSlideable) {
                     Modifier
+                        .graphicsLayer {
+                            scaleX = scale
+                            scaleY = scale
+                        }
+                        .offset {
+                            IntOffset(offsetX.toInt(), offsetY.toInt())
+                        }
                         .offset(-((settingsState.borderWidth + 1.dp)))
                         .border(
                             settingsState.borderWidth,
@@ -137,12 +210,13 @@ internal fun MainScreenDrawerContent(
                                 0.3f,
                                 DrawerDefaults.standardContainerColor
                             ),
-                            DrawerDefaults.shape
+                            shape
                         )
+                        .clip(shape)
                 } else Modifier
             ),
         drawerContainerColor = MaterialTheme.colorScheme.surfaceContainerLowest,
-        drawerShape = if (isSheetSlideable) DrawerDefaults.shape else RectangleShape,
+        drawerShape = if (isSheetSlideable) shape else RectangleShape,
         windowInsets = WindowInsets(0)
     ) {
         val focus = LocalFocusManager.current
