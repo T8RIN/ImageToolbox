831a2b33810c7434e54353843b1724c8f053f3e0
T8RIN <oolbp@bk.ru>
2024-05-25 01:55:34 +0300

    Split composables

--- feature/filters/src/main/java/ru/tech/imageresizershrinker/feature/filters/presentation/components/TemplateFilterSelectionItem.kt
+++ feature/filters/src/main/java/ru/tech/imageresizershrinker/feature/filters/presentation/components/TemplateFilterSelectionItem.kt
@@ -20,38 +20,20 @@ package ru.tech.imageresizershrinker.feature.filters.presentation.components
 import android.graphics.Bitmap
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.AutoFixHigh
-import androidx.compose.material.icons.outlined.DeleteOutline
-import androidx.compose.material.icons.outlined.Extension
 import androidx.compose.material.icons.outlined.Info
-import androidx.compose.material.icons.rounded.FilePresent
-import androidx.compose.material.icons.rounded.QrCode
-import androidx.compose.material.icons.rounded.QrCode2
-import androidx.compose.material.icons.rounded.Save
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -64,33 +46,19 @@ import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.min
-import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
 import coil.transform.Transformation
 import ru.tech.imageresizershrinker.core.filters.domain.model.TemplateFilter
 import ru.tech.imageresizershrinker.core.filters.presentation.model.UiFilter
 import ru.tech.imageresizershrinker.core.filters.presentation.model.toUiFilter
-import ru.tech.imageresizershrinker.core.filters.presentation.utils.LocalFavoriteFiltersInteractor
 import ru.tech.imageresizershrinker.core.resources.R
-import ru.tech.imageresizershrinker.core.resources.icons.EditAlt
 import ru.tech.imageresizershrinker.core.settings.presentation.provider.LocalSettingsState
 import ru.tech.imageresizershrinker.core.ui.theme.outlineVariant
-import ru.tech.imageresizershrinker.core.ui.widget.buttons.EnhancedButton
-import ru.tech.imageresizershrinker.core.ui.widget.buttons.EnhancedIconButton
-import ru.tech.imageresizershrinker.core.ui.widget.modifier.ContainerShapeDefaults
-import ru.tech.imageresizershrinker.core.ui.widget.modifier.alertDialogBorder
 import ru.tech.imageresizershrinker.core.ui.widget.modifier.shimmer
 import ru.tech.imageresizershrinker.core.ui.widget.modifier.transparencyChecker
-import ru.tech.imageresizershrinker.core.ui.widget.preferences.PreferenceItem
 import ru.tech.imageresizershrinker.core.ui.widget.preferences.PreferenceItemOverload
-import ru.tech.imageresizershrinker.core.ui.widget.sheets.SimpleSheet
-import ru.tech.imageresizershrinker.core.ui.widget.text.TitleItem
 
 @Composable
 internal fun TemplateFilterSelectionItem(
@@ -190,260 +158,4 @@ internal fun TemplateFilterSelectionItem(
         templateFilter = templateFilter,
         onRequestFilterMapping = onRequestFilterMapping
     )
-}
-
-@Composable
-internal fun FilterTemplateInfoSheet(
-    visible: Boolean,
-    onDismiss: (Boolean) -> Unit,
-    templateFilter: TemplateFilter<Bitmap>,
-    onRequestFilterMapping: ((UiFilter<*>) -> Transformation)?
-) {
-    SimpleSheet(
-        visible = visible,
-        onDismiss = onDismiss,
-        confirmButton = {
-            EnhancedButton(
-                containerColor = MaterialTheme.colorScheme.secondaryContainer,
-                onClick = {
-                    onDismiss(false)
-                }
-            ) {
-                Text(stringResource(R.string.close))
-            }
-        },
-        title = {
-            TitleItem(
-                text = stringResource(id = R.string.template_filter),
-                icon = Icons.Outlined.Extension
-            )
-        }
-    ) {
-        val context = LocalContext.current
-        val model = remember(templateFilter) {
-            if (onRequestFilterMapping != null) {
-                ImageRequest.Builder(context)
-                    .data(R.drawable.filter_preview_source)
-                    .error(R.drawable.filter_preview_source)
-                    .transformations(templateFilter.filters.map { onRequestFilterMapping(it.toUiFilter()) })
-                    .diskCacheKey(templateFilter.toString())
-                    .memoryCacheKey(templateFilter.toString())
-                    .crossfade(true)
-                    .size(300, 300)
-                    .build()
-            } else null
-        }
-        var loading by remember {
-            mutableStateOf(false)
-        }
-        val painter = rememberAsyncImagePainter(
-            model = model,
-            onLoading = {
-                loading = true
-            },
-            onSuccess = {
-                loading = false
-            }
-        )
-        var filterContent by rememberSaveable {
-            mutableStateOf("")
-        }
-        val interactor = LocalFavoriteFiltersInteractor.current
-        LaunchedEffect(filterContent) {
-            if (filterContent.isEmpty()) {
-                filterContent = interactor.convertTemplateFilterToString(templateFilter)
-            }
-        }
-
-        var showShareDialog by rememberSaveable {
-            mutableStateOf(false)
-        }
-
-        LazyColumn(
-            modifier = Modifier.fillMaxWidth(),
-            contentPadding = PaddingValues(16.dp),
-            verticalArrangement = Arrangement.spacedBy(4.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            item {
-                if (onRequestFilterMapping != null) {
-                    Spacer(modifier = Modifier.height(36.dp))
-                }
-                BoxWithConstraints(
-                    modifier = Modifier.then(
-                        if (onRequestFilterMapping != null) {
-                            Modifier
-                                .background(
-                                    color = MaterialTheme.colorScheme.surfaceContainerLowest,
-                                    shape = RoundedCornerShape(16.dp)
-                                )
-                                .padding(16.dp)
-                        } else Modifier
-                    )
-                ) {
-                    val targetSize = min(min(maxWidth, maxHeight), 300.dp)
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        QrCode(
-                            content = filterContent,
-                            modifier = Modifier
-                                .then(
-                                    if (onRequestFilterMapping != null) {
-                                        Modifier.padding(top = 36.dp, bottom = 16.dp)
-                                    } else Modifier
-                                )
-                                .size(targetSize)
-                        )
-
-                        Text(
-                            text = templateFilter.name,
-                            style = MaterialTheme.typography.headlineSmall,
-                            textAlign = TextAlign.Center,
-                            modifier = Modifier.width(targetSize)
-                        )
-                    }
-
-                    if (onRequestFilterMapping != null) {
-                        Image(
-                            painter = painter,
-                            contentScale = ContentScale.Crop,
-                            contentDescription = null,
-                            modifier = Modifier
-                                .align(Alignment.TopCenter)
-                                .offset(y = (-48).dp)
-                                .size(64.dp)
-                                .clip(MaterialTheme.shapes.medium)
-                                .transparencyChecker()
-                                .shimmer(loading)
-                        )
-                    }
-                }
-
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    modifier = Modifier.padding(top = 8.dp)
-                ) {
-                    EnhancedIconButton(
-                        onClick = { /*TODO*/ },
-                        containerColor = MaterialTheme.colorScheme.error
-                    ) {
-                        Icon(
-                            imageVector = Icons.Outlined.DeleteOutline,
-                            contentDescription = stringResource(R.string.delete)
-                        )
-                    }
-                    EnhancedButton(
-                        onClick = { showShareDialog = true },
-                        modifier = Modifier.fillMaxWidth(0.75f)
-                    ) {
-                        Text(stringResource(R.string.share))
-                    }
-                    EnhancedIconButton(
-                        onClick = { /*TODO*/ },
-                        containerColor = MaterialTheme.colorScheme.secondary
-                    ) {
-                        Icon(
-                            imageVector = Icons.Rounded.EditAlt,
-                            contentDescription = stringResource(R.string.edit)
-                        )
-                    }
-                }
-            }
-        }
-
-        if (showShareDialog) {
-            AlertDialog(
-                modifier = Modifier.alertDialogBorder(),
-                onDismissRequest = { showShareDialog = false },
-                confirmButton = {
-                    EnhancedButton(
-                        onClick = { showShareDialog = false },
-                        containerColor = MaterialTheme.colorScheme.secondaryContainer
-                    ) {
-                        Text(stringResource(R.string.cancel))
-                    }
-                },
-                title = {
-                    Text(stringResource(R.string.share))
-                },
-                icon = {
-                    Icon(
-                        imageVector = Icons.Outlined.AutoFixHigh,
-                        contentDescription = null
-                    )
-                },
-                text = {
-                    Column(
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Center
-                    ) {
-                        PreferenceItem(
-                            title = stringResource(R.string.as_qr_code),
-                            shape = ContainerShapeDefaults.topShape,
-                            startIcon = Icons.Rounded.QrCode,
-                            onClick = {
-                                showShareDialog = false
-                                TODO()
-                            },
-                            titleFontStyle = LocalTextStyle.current.copy(
-                                fontSize = 16.sp,
-                                fontWeight = FontWeight.Medium,
-                                lineHeight = 18.sp,
-                                textAlign = TextAlign.Center
-                            )
-                        )
-                        Spacer(Modifier.height(4.dp))
-                        PreferenceItem(
-                            title = stringResource(R.string.as_file),
-                            shape = ContainerShapeDefaults.centerShape,
-                            startIcon = Icons.Rounded.FilePresent,
-                            onClick = {
-                                showShareDialog = false
-                                TODO()
-                            },
-                            titleFontStyle = LocalTextStyle.current.copy(
-                                fontSize = 16.sp,
-                                fontWeight = FontWeight.Medium,
-                                lineHeight = 18.sp,
-                                textAlign = TextAlign.Center
-                            )
-                        )
-                        Spacer(Modifier.height(4.dp))
-                        PreferenceItem(
-                            title = stringResource(R.string.save_as_qr_code_image),
-                            shape = ContainerShapeDefaults.centerShape,
-                            startIcon = Icons.Rounded.QrCode2,
-                            onClick = {
-                                showShareDialog = false
-                                TODO()
-                            },
-                            titleFontStyle = LocalTextStyle.current.copy(
-                                fontSize = 16.sp,
-                                fontWeight = FontWeight.Medium,
-                                lineHeight = 18.sp,
-                                textAlign = TextAlign.Center
-                            )
-                        )
-                        Spacer(Modifier.height(4.dp))
-                        PreferenceItem(
-                            title = stringResource(R.string.save_as_file),
-                            shape = ContainerShapeDefaults.bottomShape,
-                            startIcon = Icons.Rounded.Save,
-                            onClick = {
-                                showShareDialog = false
-                                TODO()
-                            },
-                            titleFontStyle = LocalTextStyle.current.copy(
-                                fontSize = 16.sp,
-                                fontWeight = FontWeight.Medium,
-                                lineHeight = 18.sp,
-                                textAlign = TextAlign.Center
-                            )
-                        )
-                    }
-                }
-            )
-        }
-    }
 }
