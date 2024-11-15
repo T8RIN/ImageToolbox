cbecea8adef3d54b1a291ce1cf73c0ea9bea8286
T8RIN <oolbp@bk.ru>
2024-11-16 00:33:59 +0300

    Add licences to file headers

--- core/data/src/main/java/ru/tech/imageresizershrinker/core/data/saving/SaveException.kt
+++ core/data/src/main/java/ru/tech/imageresizershrinker/core/data/saving/SaveException.kt
@@ -17,5 +17,4 @@
 
 package ru.tech.imageresizershrinker.core.data.saving
 
-
 class SaveException(override val message: String?) : Throwable(message)
--- core/domain/src/main/kotlin/ru/tech/imageresizershrinker/core/domain/utils/IntUtils.kt
+++ core/domain/src/main/kotlin/ru/tech/imageresizershrinker/core/domain/utils/IntUtils.kt
@@ -17,7 +17,6 @@
 
 package ru.tech.imageresizershrinker.core.domain.utils
 
-
 fun Int.toBoolean() = this != 0
 
 fun Boolean.toInt() = if (this) 1 else 0
