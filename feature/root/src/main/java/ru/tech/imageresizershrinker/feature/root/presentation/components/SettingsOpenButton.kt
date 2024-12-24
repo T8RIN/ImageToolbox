b72984a2e796c67c3a1064d4c4abc3448796338b
T8RIN <oolbp@bk.ru>
2024-12-25 02:22:29 +0300

    Improve fast settings floating side

--- feature/root/src/main/java/ru/tech/imageresizershrinker/feature/root/presentation/components/SettingsOpenButton.kt
+++ feature/root/src/main/java/ru/tech/imageresizershrinker/feature/root/presentation/components/SettingsOpenButton.kt
@@ -26,7 +26,13 @@ import androidx.compose.animation.scaleIn
 import androidx.compose.animation.scaleOut
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -36,11 +42,14 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Settings
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 import ru.tech.imageresizershrinker.core.settings.domain.model.FastSettingsSide
@@ -65,22 +74,40 @@ internal fun BoxScope.SettingsOpenButton(
         Alignment.CenterEnd
     }
 
-    val shape = if (fastSettingsSide == FastSettingsSide.CenterStart) {
-        RoundedCornerShape(
-            topEnd = 8.dp,
-            bottomEnd = 8.dp
-        )
+    val direction = LocalLayoutDirection.current
+    val paddingValues = WindowInsets.safeDrawing.asPaddingValues()
+    val (startPadding, endPadding) = remember(paddingValues, direction) {
+        derivedStateOf {
+            paddingValues.calculateStartPadding(direction) to paddingValues.calculateEndPadding(
+                direction
+            )
+        }
+    }.value
+
+    val shape = if (startPadding == 0.dp && endPadding == 0.dp) {
+        if (fastSettingsSide == FastSettingsSide.CenterStart) {
+            RoundedCornerShape(
+                topEnd = 8.dp,
+                bottomEnd = 8.dp
+            )
+        } else {
+            RoundedCornerShape(
+                topStart = 8.dp,
+                bottomStart = 8.dp
+            )
+        }
     } else {
-        RoundedCornerShape(
-            topStart = 8.dp,
-            bottomStart = 8.dp
-        )
+        RoundedCornerShape(8.dp)
     }
 
     Surface(
         color = Color.Transparent,
         modifier = Modifier
             .align(alignment)
+            .padding(
+                start = startPadding,
+                end = endPadding
+            )
             .size(
                 height = animateDpAsState(
                     if (isWantOpenSettings) 64.dp
