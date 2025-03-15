d3ee2ef9b5ae277d287ee96c1edbbe9fb4eb557f
T8RIN <oolbp@bk.ru>
2025-03-15 21:36:05 +0300

    Added logs sending feature by #1755

--- feature/settings/src/main/java/ru/tech/imageresizershrinker/feature/settings/presentation/components/SendLogsSettingItem.kt
+++ feature/settings/src/main/java/ru/tech/imageresizershrinker/feature/settings/presentation/components/SendLogsSettingItem.kt
@@ -19,33 +19,36 @@ package ru.tech.imageresizershrinker.feature.settings.presentation.components
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.LogoDev
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.t8rin.logger.Logger
 import ru.tech.imageresizershrinker.core.resources.R
-import ru.tech.imageresizershrinker.core.resources.icons.SaveConfirm
-import ru.tech.imageresizershrinker.core.settings.presentation.provider.LocalSettingsState
+import ru.tech.imageresizershrinker.core.ui.theme.mixedContainer
+import ru.tech.imageresizershrinker.core.ui.theme.onMixedContainer
 import ru.tech.imageresizershrinker.core.ui.widget.modifier.ContainerShapeDefaults
-import ru.tech.imageresizershrinker.core.ui.widget.preferences.PreferenceRowSwitch
+import ru.tech.imageresizershrinker.core.ui.widget.preferences.PreferenceItem
 
 @Composable
-fun EnableToolExitConfirmationSettingItem(
-    onClick: () -> Unit,
+fun SendLogsSettingItem(
+    modifier: Modifier = Modifier.padding(horizontal = 8.dp),
     shape: Shape = ContainerShapeDefaults.centerShape,
-    modifier: Modifier = Modifier.padding(horizontal = 8.dp)
+    color: Color = MaterialTheme.colorScheme.mixedContainer.copy(0.9f),
+    contentColor: Color = MaterialTheme.colorScheme.onMixedContainer
 ) {
-    val settingsState = LocalSettingsState.current
-    PreferenceRowSwitch(
-        modifier = modifier,
+    PreferenceItem(
+        contentColor = contentColor,
         shape = shape,
-        title = stringResource(R.string.tool_exit_confirmation),
-        subtitle = stringResource(R.string.tool_exit_confirmation_sub),
-        checked = settingsState.enableToolExitConfirmation,
-        onClick = {
-            onClick()
-        },
-        startIcon = Icons.Outlined.SaveConfirm
+        onClick = Logger::shareLogs,
+        startIcon = Icons.Rounded.LogoDev,
+        title = stringResource(R.string.send_logs),
+        subtitle = stringResource(R.string.send_logs_sub),
+        color = color,
+        modifier = modifier
     )
 }
