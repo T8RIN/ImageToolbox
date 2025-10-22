cea7828a8197698df9c97b81d1d2966bd477e281
T8RIN <oolbp@bk.ru>
2025-10-22 23:23:33 +0300

    Added setting to turn on launcher like tools appearance mode

--- feature/main/src/main/java/com/t8rin/imagetoolbox/feature/main/presentation/components/ScreenPreferenceSelection.kt
+++ feature/main/src/main/java/com/t8rin/imagetoolbox/feature/main/presentation/components/ScreenPreferenceSelection.kt
@@ -153,7 +153,7 @@ internal fun RowScope.ScreenPreferenceSelection(
                     isGrid, navBarsPadding,
                     showClipButton, showSearchButton,
                     isSheetSlideable, layoutDirection,
-                    cutout, showNavRail
+                    cutout, showNavRail, isScreenSelectionLauncherMode
                 ) {
                     derivedStateOf {
                         PaddingValues(
