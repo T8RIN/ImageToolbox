187f5a27a443d692efc4189a5e5a357f9e3bbf8b
T8RIN <oolbp@bk.ru>
2025-10-31 12:38:34 +0300

    Improved in app UI and Icons

--- feature/settings/src/main/java/com/t8rin/imagetoolbox/feature/settings/presentation/components/DonateSettingItem.kt
+++ feature/settings/src/main/java/com/t8rin/imagetoolbox/feature/settings/presentation/components/DonateSettingItem.kt
@@ -19,7 +19,6 @@ package com.t8rin.imagetoolbox.feature.settings.presentation.components
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.VolunteerActivism
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -31,6 +30,7 @@ import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.t8rin.imagetoolbox.core.resources.R
+import com.t8rin.imagetoolbox.core.resources.icons.VolunteerActivism
 import com.t8rin.imagetoolbox.core.settings.presentation.model.isFirstLaunch
 import com.t8rin.imagetoolbox.core.settings.presentation.provider.LocalSettingsState
 import com.t8rin.imagetoolbox.core.ui.widget.modifier.ShapeDefaults
