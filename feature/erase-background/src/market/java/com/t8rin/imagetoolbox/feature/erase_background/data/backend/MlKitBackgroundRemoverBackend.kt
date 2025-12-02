14cf5dd24fd18d378f479b4eda210ee1bc03a5d7
T8RIN <oolbp@bk.ru>
2025-12-02 03:34:39 +0300

    Added auto background remover to foss version through U2Net, and ability to choose it over MlKit in market version

--- feature/erase-background/src/market/java/com/t8rin/imagetoolbox/feature/erase_background/data/backend/MlKitBackgroundRemoverBackend.kt
+++ feature/erase-background/src/market/java/com/t8rin/imagetoolbox/feature/erase_background/data/backend/MlKitBackgroundRemoverBackend.kt
@@ -20,10 +20,18 @@ package com.t8rin.imagetoolbox.feature.erase_background.data.backend
 import android.annotation.SuppressLint
 import android.graphics.Bitmap
 import android.os.Build
-import com.t8rin.imagetoolbox.feature.erase_background.data.backend.impl.MlKitBackgroundRemover
-import com.t8rin.imagetoolbox.feature.erase_background.data.backend.impl.MlKitSubjectBackgroundRemover
+import androidx.annotation.RequiresApi
+import androidx.core.graphics.set
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.segmentation.Segmentation
+import com.google.mlkit.vision.segmentation.Segmenter
+import com.google.mlkit.vision.segmentation.selfie.SelfieSegmenterOptions
+import com.google.mlkit.vision.segmentation.subject.SubjectSegmentation
+import com.google.mlkit.vision.segmentation.subject.SubjectSegmenter
+import com.google.mlkit.vision.segmentation.subject.SubjectSegmenterOptions
 import com.t8rin.imagetoolbox.feature.erase_background.domain.AutoBackgroundRemoverBackend
 import kotlinx.coroutines.suspendCancellableCoroutine
+import java.nio.ByteBuffer
 import kotlin.coroutines.resume
 
 internal object MlKitBackgroundRemoverBackend : AutoBackgroundRemoverBackend<Bitmap> {
@@ -74,6 +82,116 @@ internal object MlKitBackgroundRemoverBackend : AutoBackgroundRemoverBackend<Bit
         companion object Companion {
             val Best: ApiType get() = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) New else Old
         }
+    }
+
+}
+
+private object MlKitBackgroundRemover {
+
+    private var segment: Segmenter? = null
+    private var buffer = ByteBuffer.allocate(0)
+    private var width = 0
+    private var height = 0
+
+
+    init {
+        runCatching {
+            val segmentOptions = SelfieSegmenterOptions.Builder()
+                .setDetectorMode(SelfieSegmenterOptions.SINGLE_IMAGE_MODE)
+                .build()
+            segment = Segmentation.getClient(segmentOptions)
+        }
+    }
+
+
+    /**
+     * Process the image to get buffer and image height and width
+     * @param bitmap Bitmap which you want to remove background.
+     * @param onFinish listener for success and failure callback.
+     **/
+    fun removeBackground(
+        bitmap: Bitmap,
+        onFinish: (Result<Bitmap>) -> Unit
+    ) {
+        //Generate a copy of bitmap just in case the if the bitmap is immutable.
+        val copyBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+        val input = InputImage.fromBitmap(copyBitmap, 0)
+        val segmenter = segment ?: return onFinish(Result.failure(NoClassDefFoundError()))
+
+        segmenter.process(input)
+            .addOnSuccessListener { segmentationMask ->
+                buffer = segmentationMask.buffer
+                width = segmentationMask.width
+                height = segmentationMask.height
+
+                val resultBitmap = removeBackgroundFromImage(copyBitmap)
+                onFinish(Result.success(resultBitmap))
+            }
+            .addOnFailureListener { e ->
+                onFinish(Result.failure(e))
+            }
+    }
+
+
+    /**
+     * Change the background pixels color to transparent.
+     * */
+    private fun removeBackgroundFromImage(
+        image: Bitmap
+    ): Bitmap {
+        image.setHasAlpha(true)
+        for (y in 0 until height) {
+            for (x in 0 until width) {
+                val bgConfidence = ((1.0 - buffer.float) * 255).toInt()
+                if (bgConfidence >= 100) {
+                    image[x, y] = 0
+                }
+            }
+        }
+        buffer.rewind()
+
+        return image
+    }
+
+}
+
+@RequiresApi(api = Build.VERSION_CODES.N)
+private object MlKitSubjectBackgroundRemover {
+
+    private var segment: SubjectSegmenter? = null
+
+    init {
+        runCatching {
+            val segmentOptions = SubjectSegmenterOptions.Builder()
+                .enableForegroundBitmap()
+                .build()
+            segment = SubjectSegmentation.getClient(segmentOptions)
+        }
+    }
+
+
+    /**
+     * Process the image to get buffer and image height and width
+     * @param bitmap Bitmap which you want to remove background.
+     * @param onFinish listener for success and failure callback.
+     **/
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    fun removeBackground(
+        bitmap: Bitmap,
+        onFinish: (Result<Bitmap>) -> Unit
+    ) {
+        //Generate a copy of bitmap just in case the if the bitmap is immutable.
+        val copyBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+        val input = InputImage.fromBitmap(copyBitmap, 0)
+        val segmenter = segment ?: return onFinish(Result.failure(NoClassDefFoundError()))
+
+        segmenter.process(input)
+            .addOnSuccessListener {
+                onFinish(Result.success(it?.foregroundBitmap ?: bitmap))
+            }
+            .addOnFailureListener { e ->
+                onFinish(Result.failure(e))
+            }
     }
 
 }
