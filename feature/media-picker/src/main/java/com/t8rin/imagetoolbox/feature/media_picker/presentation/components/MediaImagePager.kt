75e014e400287923c1d1c5e33bf0a2fa96b6ea41
T8RIN <oolbp@bk.ru>
2026-01-09 19:49:27 +0300

    fix

--- feature/media-picker/src/main/java/com/t8rin/imagetoolbox/feature/media_picker/presentation/components/MediaImagePager.kt
+++ feature/media-picker/src/main/java/com/t8rin/imagetoolbox/feature/media_picker/presentation/components/MediaImagePager.kt
@@ -1,6 +1,6 @@
 /*
  * ImageToolbox is an image editor for android
- * Copyright (c) 2025 T8RIN (Malik Mukhametzyanov)
+ * Copyright (c) 2026 T8RIN (Malik Mukhametzyanov)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -222,7 +222,7 @@ internal fun MediaImagePager(
                     Box(
                         modifier = Modifier.fillMaxSize()
                     ) {
-                        val zoomState = rememberZoomState(10f)
+                        val zoomState = rememberZoomState(20f)
                         Picture(
                             showTransparencyChecker = false,
                             model = media.getOrNull(page)?.uri,
