69fdae722d21dda3f5f65a416662ded5328a1237
T8RIN <oolbp@bk.ru>
2026-01-12 01:53:10 +0300

    Improvements and polishments

--- core/ui/src/main/kotlin/com/t8rin/imagetoolbox/core/ui/widget/utils/ScreenList.kt
+++ core/ui/src/main/kotlin/com/t8rin/imagetoolbox/core/ui/widget/utils/ScreenList.kt
@@ -1,6 +1,6 @@
 /*
  * ImageToolbox is an image editor for android
- * Copyright (c) 2024 T8RIN (Malik Mukhametzyanov)
+ * Copyright (c) 2026 T8RIN (Malik Mukhametzyanov)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -118,6 +118,7 @@ internal fun List<Uri>.screenList(
                 Screen.Filter(
                     type = Screen.Filter.Type.Masking(uris.firstOrNull())
                 ),
+                Screen.AiTools(uris),
                 Screen.MarkupLayers(uris.firstOrNull()),
                 Screen.Watermarking(uris),
                 Screen.ImageStitching(uris),
