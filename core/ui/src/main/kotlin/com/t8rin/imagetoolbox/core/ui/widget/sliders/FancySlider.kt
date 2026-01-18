53854bed92deb4a530270985075e8ba6536a6d18
T8RIN <oolbp@bk.ru>
2026-01-18 14:05:21 +0300

    Improved UI

--- core/ui/src/main/kotlin/com/t8rin/imagetoolbox/core/ui/widget/sliders/FancySlider.kt
+++ core/ui/src/main/kotlin/com/t8rin/imagetoolbox/core/ui/widget/sliders/FancySlider.kt
@@ -1,6 +1,6 @@
 /*
  * ImageToolbox is an image editor for android
- * Copyright (c) 2024 T8RIN (Malik Mukhametzyanov)
+ * Copyright (c) 2026 T8RIN (Malik Mukhametzyanov)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -35,20 +35,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.graphics.TransformOrigin
 import androidx.compose.ui.graphics.compositeOver
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import com.t8rin.imagetoolbox.core.settings.presentation.provider.LocalSettingsState
@@ -59,7 +51,6 @@ import com.t8rin.imagetoolbox.core.ui.utils.helper.rememberRipple
 import com.t8rin.imagetoolbox.core.ui.utils.provider.SafeLocalContainerColor
 import com.t8rin.imagetoolbox.core.ui.widget.modifier.container
 import com.t8rin.imagetoolbox.core.ui.widget.modifier.materialShadow
-import com.t8rin.imagetoolbox.core.ui.widget.modifier.trackOverslide
 import com.t8rin.imagetoolbox.core.ui.widget.sliders.custom_slider.CustomRangeSlider
 import com.t8rin.imagetoolbox.core.ui.widget.sliders.custom_slider.CustomSlider
 import com.t8rin.imagetoolbox.core.ui.widget.sliders.custom_slider.CustomSliderColors
@@ -114,21 +105,11 @@ fun FancySlider(
 
     val settingsState = LocalSettingsState.current
     LocalMinimumInteractiveComponentSize.ProvidesValue(Dp.Unspecified) {
-        var scaleX by remember { mutableFloatStateOf(1f) }
-        var scaleY by remember { mutableFloatStateOf(1f) }
-        var translateX by remember { mutableFloatStateOf(0f) }
-        var transformOrigin by remember { mutableStateOf(TransformOrigin.Center) }
         CustomSlider(
             interactionSource = interactionSource,
             thumb = thumb,
             enabled = enabled,
             modifier = modifier
-                .graphicsLayer {
-                    this.transformOrigin = transformOrigin
-                    this.scaleX = scaleX
-                    this.scaleY = scaleY
-                    this.translationX = translateX
-                }
                 .then(
                     if (drawContainer) {
                         Modifier
@@ -162,43 +143,11 @@ fun FancySlider(
             valueRange = valueRange,
             steps = steps,
             track = { sliderState ->
-                val sliderFraction by remember {
-                    derivedStateOf {
-                        (value - sliderState.valueRange.start) / (sliderState.valueRange.endInclusive - sliderState.valueRange.start)
-                    }
-                }
-
-                val density = LocalDensity.current
-                val isLtr = LocalLayoutDirection.current == LayoutDirection.Ltr
-
                 CustomSliderDefaults.Track(
                     sliderState = sliderState,
                     colors = colors.toCustom(),
                     trackHeight = 38.dp,
-                    enabled = enabled,
-                    modifier = Modifier.trackOverslide(value = sliderFraction) { overslide ->
-                        transformOrigin = TransformOrigin(
-                            pivotFractionX = when (isLtr) {
-                                true -> if (sliderFraction < .5f) 2f else -1f
-                                false -> if (sliderFraction < .5f) -1f else 2f
-                            },
-                            pivotFractionY = .5f,
-                        )
-
-                        when (sliderFraction) {
-                            in 0f..(.5f) -> {
-                                scaleY = 1f + overslide / 2f
-                                scaleX = 1f - (overslide * .0025f)
-                            }
-
-                            else -> {
-                                scaleY = 1f - overslide / 2f
-                                scaleX = 1f + (overslide * .0025f)
-                            }
-                        }
-
-                        translateX = overslide * with(density) { 24.dp.toPx() }
-                    }
+                    enabled = enabled
                 )
             }
         )
@@ -227,21 +176,11 @@ fun FancyRangeSlider(
 
     val settingsState = LocalSettingsState.current
     LocalMinimumInteractiveComponentSize.ProvidesValue(Dp.Unspecified) {
-        var scaleX by remember { mutableFloatStateOf(1f) }
-        var scaleY by remember { mutableFloatStateOf(1f) }
-        var translateX by remember { mutableFloatStateOf(0f) }
-        var transformOrigin by remember { mutableStateOf(TransformOrigin.Center) }
         CustomRangeSlider(
             startInteractionSource = startInteractionSource,
             endInteractionSource = endInteractionSource,
             enabled = enabled,
             modifier = modifier
-                .graphicsLayer {
-                    this.transformOrigin = transformOrigin
-                    this.scaleX = scaleX
-                    this.scaleY = scaleY
-                    this.translationX = translateX
-                }
                 .then(
                     if (drawContainer) {
                         Modifier
