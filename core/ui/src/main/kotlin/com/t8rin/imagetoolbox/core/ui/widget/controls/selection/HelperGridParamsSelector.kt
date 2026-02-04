b78b3d21ee1b6e9f4a75b730769a96bf3a3e1ec0
T8RIN <oolbp@bk.ru>
2026-02-04 23:39:45 +0300

    Added ability to enable primary lines for helper grid by #2489

--- core/ui/src/main/kotlin/com/t8rin/imagetoolbox/core/ui/widget/controls/selection/HelperGridParamsSelector.kt
+++ core/ui/src/main/kotlin/com/t8rin/imagetoolbox/core/ui/widget/controls/selection/HelperGridParamsSelector.kt
@@ -1,6 +1,6 @@
 /*
  * ImageToolbox is an image editor for android
- * Copyright (c) 2024 T8RIN (Malik Mukhametzyanov)
+ * Copyright (c) 2026 T8RIN (Malik Mukhametzyanov)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,19 +26,27 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ColorLens
+import androidx.compose.material.icons.outlined.LineWeight
 import androidx.compose.material.icons.outlined.TableRows
 import androidx.compose.material.icons.outlined.ViewColumn
+import androidx.compose.material.icons.rounded.FormatLineSpacing
 import androidx.compose.material.icons.rounded.GridOn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.smarttoolfactory.extendedcolors.util.roundToTwoDigits
 import com.t8rin.imagetoolbox.core.resources.R
+import com.t8rin.imagetoolbox.core.ui.theme.ImageToolboxThemeForPreview
 import com.t8rin.imagetoolbox.core.ui.theme.toColor
 import com.t8rin.imagetoolbox.core.ui.widget.enhanced.EnhancedSliderItem
 import com.t8rin.imagetoolbox.core.ui.widget.modifier.HelperGridParams
@@ -93,6 +101,22 @@ fun HelperGridParamsSelector(
                     title = stringResource(R.string.grid_color)
                 )
                 EnhancedSliderItem(
+                    value = value.linesWidth,
+                    title = stringResource(R.string.line_width),
+                    icon = Icons.Outlined.LineWeight,
+                    internalStateTransformation = {
+                        it.roundToTwoDigits()
+                    },
+                    valueSuffix = " Pt",
+                    shape = ShapeDefaults.center,
+                    onValueChange = {
+                        onValueChange(value.copy(linesWidth = it))
+                    },
+                    valueRange = 0f..1.5f,
+                    containerColor = MaterialTheme.colorScheme.surface,
+                    modifier = Modifier.padding(horizontal = 8.dp)
+                )
+                EnhancedSliderItem(
                     value = value.cellWidth,
                     title = stringResource(R.string.cell_width),
                     icon = Icons.Outlined.ViewColumn,
@@ -116,7 +140,7 @@ fun HelperGridParamsSelector(
                         it.roundToTwoDigits()
                     },
                     valueSuffix = " Pt",
-                    shape = ShapeDefaults.bottom,
+                    shape = ShapeDefaults.center,
                     onValueChange = {
                         onValueChange(value.copy(cellHeight = it))
                     },
@@ -124,8 +148,33 @@ fun HelperGridParamsSelector(
                     containerColor = MaterialTheme.colorScheme.surface,
                     modifier = Modifier.padding(horizontal = 8.dp)
                 )
+                PreferenceRowSwitch(
+                    startIcon = Icons.Rounded.FormatLineSpacing,
+                    drawContainer = true,
+                    shape = ShapeDefaults.bottom,
+                    checked = value.withPrimaryLines,
+                    onClick = {
+                        onValueChange(value.copy(withPrimaryLines = it))
+                    },
+                    title = stringResource(R.string.primary_lines),
+                    subtitle = stringResource(R.string.primary_lines_sub),
+                    containerColor = MaterialTheme.colorScheme.surface,
+                    modifier = Modifier.padding(horizontal = 8.dp)
+                )
                 Spacer(Modifier.height(4.dp))
             }
         }
     }
+}
+
+@Composable
+@Preview
+private fun Preview() = ImageToolboxThemeForPreview(false) {
+    var value by remember {
+        mutableStateOf(HelperGridParams(enabled = true))
+    }
+    HelperGridParamsSelector(
+        value = value,
+        onValueChange = { value = it }
+    )
 }
