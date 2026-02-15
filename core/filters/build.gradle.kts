783912ab40fbc6b14a3652e4ddee43c04d51ee2a
T8RIN <oolbp@bk.ru>
2026-02-15 22:22:34 +0300

    fixes to gradle

--- core/filters/build.gradle.kts
+++ core/filters/build.gradle.kts
@@ -15,25 +15,6 @@
  * along with this program.  If not, see <http://www.apache.org/licenses/LICENSE-2.0>.
  */
 
-import com.t8rin.imagetoolbox.implementation
-
-/*
- * ImageToolbox is an image editor for android
- * Copyright (c) 2024 T8RIN (Malik Mukhametzyanov)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * You should have received a copy of the Apache License
- * along with this program.  If not, see <http://www.apache.org/licenses/LICENSE-2.0>.
- */
-
 plugins {
     alias(libs.plugins.image.toolbox.library)
     alias(libs.plugins.image.toolbox.compose)
@@ -48,7 +29,7 @@ dependencies {
     implementation(projects.core.resources)
     implementation(projects.core.settings)
     implementation(projects.core.utils)
-    
+
     implementation(libs.kotlin.reflect)
     implementation(libs.toolbox.curves)
     implementation(libs.toolbox.ascii)
