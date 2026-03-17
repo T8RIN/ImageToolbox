7479fbac4e758675324983b232d2afcee73d256a
T8RIN <oolbp@bk.ru>
2026-03-17 21:14:21 +0300

    bump libs

--- app/src/main/java/com/t8rin/imagetoolbox/app/presentation/components/functions/SetupFlags.kt
+++ app/src/main/java/com/t8rin/imagetoolbox/app/presentation/components/functions/SetupFlags.kt
@@ -1,6 +1,6 @@
 /*
  * ImageToolbox is an image editor for android
- * Copyright (c) 2025 T8RIN (Malik Mukhametzyanov)
+ * Copyright (c) 2026 T8RIN (Malik Mukhametzyanov)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,10 +19,10 @@ package com.t8rin.imagetoolbox.app.presentation.components.functions
 
 import androidx.compose.foundation.ComposeFoundationFlags.isPausableCompositionInPrefetchEnabled
 import androidx.compose.material3.ComposeMaterial3Flags.isCheckboxStylingFixEnabled
-import com.arkivanov.decompose.DecomposeExperimentFlags.duplicateConfigurationsEnabled
+import com.arkivanov.decompose.DecomposeSettings
 
 internal fun setupFlags() {
     isCheckboxStylingFixEnabled = true
-    duplicateConfigurationsEnabled = true
+    DecomposeSettings.update { it.copy(duplicateConfigurationsEnabled = true) }
     isPausableCompositionInPrefetchEnabled = true
 }
