21d5cd8d3bfc33f64a81c21f00eeb5b636299f2a
Malik Mukhametzyanov <oolbp@bk.ru>
2026-09-20 14:00:49 +0300

    Fixed crash with multiple custom fonts by #3162

--- feature/settings/src/main/java/com/t8rin/imagetoolbox/feature/settings/presentation/components/additional/PickFontFamilySheet.kt
+++ feature/settings/src/main/java/com/t8rin/imagetoolbox/feature/settings/presentation/components/additional/PickFontFamilySheet.kt
@@ -31,7 +31,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import com.t8rin.imagetoolbox.core.resources.Icons
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -40,6 +39,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.t8rin.imagetoolbox.core.domain.model.MimeType
+import com.t8rin.imagetoolbox.core.resources.Icons
 import com.t8rin.imagetoolbox.core.resources.R
 import com.t8rin.imagetoolbox.core.resources.icons.Extension
 import com.t8rin.imagetoolbox.core.resources.icons.FileExport
@@ -165,7 +165,7 @@ internal fun PickFontFamilySheet(
                     }
                     items(
                         items = customEntries,
-                        key = { it.name ?: "sys" }
+                        key = { it.filePath }
                     ) { font ->
                         FontItem(
                             font = font,
